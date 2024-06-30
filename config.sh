df479a98e20028e65e4900d8244d56761b2cf950
aviadlevy <aviadlevy1@gmail.com>
2024-06-30 20:49:58 +0300

    create empty folders if not exists

--- config.sh
+++ config.sh
@@ -1,5 +1,12 @@
 #! /bin/zsh
 
+FOLDERS=(.config/ .config/alacritty/themes)
+
+for folder in $(echo ${FOLDERS[*]});
+do
+	mkdir -p ~/$(echo $folder)
+done
+
 DOTFILES=(.zshrc .kubectl_aliases .vimrc .config/alacritty/alacritty.toml .config/alacritty/themes/night_owl.toml .config/starship.toml .customterminalrc .programlangrc .gitconfig)
 
 for dotfile in $(echo ${DOTFILES[*]});
