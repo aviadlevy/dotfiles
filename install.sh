48218c98f57486b2750fb12da9aac5d5d5aaac75
aviadlevy <aviadlevy1@gmail.com>
2024-07-02 11:04:30 +0300

    fix installation script

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ command -v brew >/dev/null 2>&1 || \
 	(echo '🍺  Installing Homebrew' && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 	
 command -v brew >/dev/null 2>&1 || \
-	(echo '    Adding Homebrew to source' && echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /root/.bashrc && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" && source ~/.bashrc)
+	(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /root/.bashrc);  (eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"; source ~/.bashrc)
 
 
 # Copy Brewfile
