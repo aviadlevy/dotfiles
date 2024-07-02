a0c36386adf6fbc52f9ab077b954c6cb57e9f470
aviadlevy <aviadlevy1@gmail.com>
2024-07-02 11:38:47 +0300

    fix installation script

--- install.sh
+++ install.sh
@@ -2,11 +2,6 @@
 
 set -eufo pipefail
 
-echo ""
-echo "🤚  This script will setup .dotfiles for you."
-read -n 1 -r -s -p $'    Press any key to continue or Ctrl+C to abort...\n\n'
-
-
 # Install Homebrew
 command -v brew >/dev/null 2>&1 || \
 	(echo '🍺  Installing Homebrew' && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
